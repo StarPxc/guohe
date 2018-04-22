c0f88e55b83871e4b35546d22cc0cfce51f3da94
Ethanp <18852897377@163.com>
2018-04-22 08:44:34 +0800

    Update public_var.py

--- util/public_var.py
+++ util/public_var.py
@@ -11,6 +11,6 @@
 
 '''
 class publicVar(object):
-    LOG_FILE_NAME=r'e://guohe.log'#'/var/www/log/guohe.log'
+    LOG_FILE_NAME=r'/var/www/log/guohe.log'#'/var/www/log/guohe.log'
     DB_PASSWORD='935377012'
     DB_PASSWORD2 = 'root'
